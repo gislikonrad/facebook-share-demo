3e95a57d04225e77e6c3be9d9790ee4811be5f69
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2014-04-15 21:33:53 +0000

    changed some stuff

--- server.js
+++ server.js
@@ -38,5 +38,9 @@ app.get('/', function(req, res){
 	
 });
 
-app.listen(3000);
-
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
+
